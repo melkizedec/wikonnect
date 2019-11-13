6208b7df5425ec04ef7c3e392529e9a14c157f5e
Proverbial Ninja <kariuki.marima@gmail.com>
2019-11-13 12:37:46 +0300

    staged login & signup

--- frontend/app/routes/login.js
+++ frontend/app/routes/login.js
@@ -2,11 +2,11 @@ import Route from '@ember/routing/route';
 import { inject } from '@ember/service';
 
 export default class LoginRoute extends Route {
-    @inject
-    me;
+  @inject
+  me;
 
-    model() {
-      return this.store.createRecord('user');
-    }
+  model() {
+    return this.store.createRecord('user');
+  }
 
 }
--- frontend/app/routes/signup.js
+++ frontend/app/routes/signup.js
@@ -2,10 +2,10 @@ import Route from '@ember/routing/route';
 import { inject } from '@ember/service';
 
 export default class SignupRoute extends Route {
-    @inject
-    me;
+  @inject
+  me;
 
-    model() {
-      return this.store.createRecord('user');
-    }
+  model() {
+    return this.store.createRecord('user');
+  }
 }
